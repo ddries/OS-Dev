a97ac9d6650d03b038ba965ea37bad51efc78714
DriesCode <adri7escalona@gmail.com>
2018-11-14 19:15:43 +0100

    New ASM file with screen-related routines

--- boot.asm
+++ boot.asm
@@ -41,24 +41,10 @@ mov sp, bp
 mov ss, ax
 sti
 
-; Cursor config
-
-mov ah, 00h
-mov al, 02h
-int 10h
-
-mov ah, 02h
-mov bh, 0x00
-xor dx, dx
-int 10h
-
-; Video Mode config
-
-mov ah, 00h
-mov al, 02h
-int 10h
-
 ; Main code
+
+call video_mode_config ; Video Mode configuration
+call cursor_config ; Cursor configuration
 
 mov si, loading_str
 call print_str
@@ -67,57 +53,7 @@ jmp $ ; halt computer
 
 ; End of main code
 
-; Print routine
-
-print_str:
-pusha
-mov ah, 0Ah
-xor bx, bx
-mov cx, 1
-
-.print_ch:
-mov al, [si]
-cmp al, 0
-je .end
-cmp al, 13
-je .nwln
-int 10h
-call .move_cursor
-inc si
-jmp .print_ch
-
-.end:
-popa
-ret
-
-.move_cursor:
-pusha
-mov ah, 03h ; Read cursor position
-xor bx, bx
-int 10h
-
-mov ah, 02h ; Move cursor
-xor bx, bx
-inc dl
-
-int 10h
-
-popa
-ret
-
-.nwln:
-pusha
-mov ah, 03h
-xor bx, bx
-int 10h
-
-mov ah, 02h
-xor bx, bx
-inc dh
-mov dl, 0x0
-int 10h
-popa 
-ret
+%include "screen.asm"
 
 ; Data
 
