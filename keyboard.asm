7c5301c671fe151d54cecd104a83d7201c7fc867
DriesCode <adri7escalona@gmail.com>
2018-12-24 19:07:08 +0100

    Added protected mode support

--- keyboard.asm
+++ keyboard.asm
@@ -7,16 +7,13 @@ xor ax, ax
 int 16h ; Read keyboard
 
 cmp ah, 3Bh ; F1
-je .load_kernel
+je load_kernel ; load_kernel routine is defined in external file "32b.asm"
 
 cmp ah, 3Ch ; F2
 je .load_shell
 
 popa
 ret
-
-.load_kernel:
-jmp $
 
 .load_shell:
 call reset_device
@@ -27,6 +24,6 @@ jmp bx ; Jump to shell
 
 
 %include "disk.asm"
-
+%include "32b.asm"
 
 
