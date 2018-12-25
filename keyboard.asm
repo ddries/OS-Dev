37f97e097f36ea3f9ca0704b3db50bf698165620
DriesCode <adri7escalona@gmail.com>
2018-12-25 04:45:48 +0100

    test

--- keyboard.asm
+++ keyboard.asm
@@ -7,7 +7,7 @@ xor ax, ax
 int 16h ; Read keyboard
 
 cmp ah, 3Bh ; F1
-je load_kernel ; load_kernel routine is defined in external file "32b.asm"
+je .ld_kernel ; load_kernel routine is defined in external file "32b.asm"
 
 cmp ah, 3Ch ; F2
 je .load_shell
@@ -22,6 +22,9 @@ call load_shell
 pop bx
 jmp bx ; Jump to shell
 
+.ld_kernel:
+call video_mode_config  ; Clear screen by reseting video mode configuration
+jmp load_kernel ; Jump to kernel
 
 %include "disk.asm"
 %include "32b.asm"
